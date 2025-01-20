44c4385c89b3134f5b72ecba56042f344b0ac1df
vassia38 <alex.19vasile@gmail.com>
2025-01-20 19:54:50 +0100

    a

--- 4.DNS_DHCP/Shut_Infra.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Script pour supprimer les conteneurs du TP rapidement
-containers="C1 C2 C3 R1 R2 R3 DHCP DNSMaster DNSSlave"
-for container in $containers; do
-    lxc stop $container
-    lxc delete $container
-done
--- 4.DNS_DHCP/Shut_Network.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Script pour supprimer les réseaux du TP rapidement
-networks="net1 net2 net3 netR1-R2 netR1-R3 netR2-R3"
-for network in $networks; do
-    lxc network delete $network
-done
